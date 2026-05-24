cdd723e6eb6aff98dc3e0b916603b48442c6781d
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-24 12:27:34 +0100

    Add files via upload

--- pie.py
+++ pie.py
@@ -10,11 +10,11 @@ class myapps:
         self.root.configure(background="black")
         self.canvas= tk.Canvas(background="black",width=480,height=480)
         self.canvas.pack(padx=0,pady=0)
-        self.colors=["blue","red","green","yellow","black"]
+        self.colors=["black","blue","red","green","yellow"]
         self.angle=359.99
         canv=self.canvas
-        for a in range(len(self.colors)-1):
-            canv.create_arc(0,0,480,480,start=0,extent=self.angle-(a*80),fill=self.colors[a])
+        for a in range(len(self.colors)):
+            canv.create_arc(0,0,480,480,start=0,extent=self.angle-(a*60),fill=self.colors[a])
  
 
 
