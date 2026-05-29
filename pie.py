adf696567ac70459d6b0cea24cd9309fca86a2c4
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-29 23:19:21 +0100

    Add files via upload

--- pie.py
+++ pie.py
@@ -3,24 +3,38 @@ import tkinter as tk
 
 
 class myapps:
-    def __init__(self,root:tk.Tk):
+    def __init__(self,root:tk.Tk,backgrounds:str,colors:list,widths:int,heights:int,values:list,titles:str):
         self.root=root
-        self.root.title("pie")
-        self.root.geometry("480x480")
-        self.root.configure(background="black")
-        self.canvas= tk.Canvas(background="black",width=480,height=480)
+        self.root.title(titles)
+        self.root.geometry(str(widths)+"x"+str(heights))
+        self.root.configure(background=backgrounds)
+        self.canvas= tk.Canvas(background=backgrounds,width=widths,height=heights)
         self.canvas.pack(padx=0,pady=0)
-        self.colors=["blue","red","green","yellow","white""black"]
+        self.colors=colors
         self.angle=359.99
         canv=self.canvas
-        
-        for a in range(len(self.colors)-1):
-            canv.create_arc(0,0,480,480,start=0,extent=self.angle-(a*82),fill=self.colors[a])
- 
+        angl=0
+        aaa=0
+        for a in values:
+            aa:float=3.60 * float(a)
+            if aaa>=len(self.colors):
+                aaa=len(self.colors)-1
+            canv.create_arc(0,0,widths,heights,start=angl,extent=aa,fill=self.colors[aaa])
+            angl=angl+aa
+            aaa=aaa+1
 
 
 
+def starts(backgrounds:str,colors:list,widths:int,heights:int,values:int,titles:str):
+    root=tk.Tk()
+    apps=myapps(root,backgrounds,colors,widths,heights,values,titles)
+    root.mainloop()
 
-root=tk.Tk()
-apps=myapps(root)
-root.mainloop()
+titles="pie"
+backgrounds="black"
+widths=480
+heights=480
+colors=["blue","red","green","yellow","white","black"]
+values=[20,20,20,20,20]
+
+starts(backgrounds,colors,widths,heights,values,titles)
