1707a14641100fdf2d2b64cb522facc9ce71f07c
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-14 13:12:09 +0100

    Add files via upload

--- cd.sh
+++ cd.sh
@@ -24,6 +24,7 @@ bcc -x -i -L -Md color.c -o color.com
 bcc -x -i -L -Md more.c -o more.com
 bcc -x -i -L -Md ls.c -o ls.com
 bcc -x -i -L -Md pointer.c -o pointer.com
+bcc -x -i -L -Md printf.c -o printf.com
 
 cp *.lst CD_root/isolinux/
 
